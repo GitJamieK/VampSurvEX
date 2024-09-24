8a18e2de355988f0c948bd274065d36eec596ed5
JamieK <32877208+GitJamieK@users.noreply.github.com>
2024-09-24 12:11:02 +0200

    Added sprites+xpBar logic

---
+++ Assets/Scripts/GameManager/lvlBar/progBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class progBar : MonoBehaviour {
+    public int min;
+    public int max;
+    public int curr;
+    public Image mask;
+
+    void Start() { //subscribe event
+        ExpManager.Instance.onExpChange += handleExpChange;
+        UpdateBar();
+    }
+    public void UpdateBar() {
+        float curOffset = curr - min;
+        float maxOffset = max - min;
+        mask.fillAmount = curOffset / maxOffset;
+    }
+    void handleExpChange(int amount) {
+        curr += amount; // Update current XP
+        UpdateBar();
+    }
+    void OnDisable() { //unsubscribe from event
+        if (ExpManager.Instance != null) {
+            ExpManager.Instance.onExpChange -= handleExpChange;
+        }
+    }
+}
--- Assets/Scripts/player/player.cs
+++ Assets/Scripts/player/player.cs
@@ -10,17 +10,25 @@ public class playerUpdate : MonoBehaviour {
     [SerializeField] public int curExp;
     [SerializeField] public int maxExp;
     [SerializeField] public int curLevel;
+    progBar xpBar;
     
     void OnEnable() { //subscribe event
         ExpManager.Instance.onExpChange += handleExpChange;
     }
     void Start() {
         health = maxHealth;
+        xpBar = FindObjectOfType<progBar>(); // Find the progress bar in the scene
+        xpBar.min = 0; // Set the min value to 0 for XP
+        xpBar.max = maxExp; // Set the max value for the progress bar
+        xpBar.curr = curExp; // Set the current XP
+        xpBar.UpdateBar(); // Initialize the progress bar display
     }
-    void handleExpChange(int newExp) {
+    public void handleExpChange(int newExp) {
         curExp += newExp;
         if (curExp >= maxExp) {levelUp();}
         // add ui to show level up logic
+        xpBar.curr = curExp;
+        xpBar.UpdateBar();
     }
     void levelUp() {
         /* temp?? -> */ maxHealth += 10;
@@ -28,6 +36,9 @@ public class playerUpdate : MonoBehaviour {
         curLevel++;
         curExp = 0; //reset xp on player
         maxExp += 100; //increase xp needed for next level.
+        xpBar.max = maxExp; // Update the progress bar's max value
+        xpBar.curr = curExp; // Reset progress bar's current XP
+        xpBar.UpdateBar(); // Update the progress bar
     }
     public void takeDamage(int someDamage) {
         health -= someDamage;
